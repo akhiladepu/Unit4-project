43aa6b7e1e17621945113481d9a9ba77b3ff14df
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-09-29 00:00:20 +0530

    carasoule functionality done

--- client/src/Components/ViewYourAdd/ViewYourAdd.jsx
+++ client/src/Components/ViewYourAdd/ViewYourAdd.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 export const ViewYourAdd = () => {
+
+    const [picIndex, setPicIndex] = useState(0);
+
+    const arr = ["https://cloudfront-us-east-2.images.arcpublishing.com/reuters/INW62ON2YNNWHKBCMY6IFIETG4.jpg","https://i2-prod.manchestereveningnews.co.uk/incoming/article21678284.ece/ALTERNATES/s615/1_GettyImages-1342424026.jpg","https://images.indianexpress.com/2021/09/cristiano-ronaldo-manchester-united.jpg","https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTgzNDQ5NTQyMjQ5Njg2NDM0/cristiano-ronaldo-man-united-return.jpg","https://cdn.wionews.com/sites/default/files/styles/story_page/public/2020/06/05/143417-untitled-design-54.jpg"];
+
+    const AddImg = () => {
+        if (picIndex === arr.length - 1) {
+            setPicIndex(0);
+        }
+        else
+        {
+            setPicIndex(picIndex + 1);
+        }
+    }
+
+    const DecImg = () => {
+        
+        if (picIndex === 0) {
+            setPicIndex(arr.length - 1);
+        }
+        else
+        {
+            setPicIndex(picIndex - 1);
+        }
+    }
+
     return (
         <div className="col-12" style={{ margin: '10px auto' }}>
             
@@ -11,11 +37,24 @@ export const ViewYourAdd = () => {
             {/* 2nd row start */}
             <div className="col-12 d-flex flex-row" style={{ margin: '10px auto' }}>
 
-                <div className="d-flex flex-column" style={{width:"37.43%",marginLeft: '3.89%',marginRight:"1.67%",border: '1px solid black'}}>
-                    <div>
-
+                <div className="d-flex flex-column" style={{width:"37.43%",marginLeft: '3.89%',marginRight:"1.67%"}}>
+                    <div style={{position:"relative"}}>
+                        <button onClick={AddImg} style={{ outline: "none",border: "none",background: "rgb(255,255,255,0.7)", width: "40px", height: "40px", justifyContent: "center", borderRadius: "50%", position: "absolute", right: "3%", top: "40%" }}><img src={`SliderLeft.svg`} alt="" style={{position: "absolute", top:"30%",right:"30%",left:"40%",bottom:"30%"}} /></button>
+                        <img src={arr[picIndex]} className="img-fluid" style={{ height: "345px", width: "100%" }} alt="" />
+                        <button  onClick={DecImg} style={{outline: "none",border: "none",background: "rgb(255,255,255,0.7)", width: "40px", height: "40px", justifyContent: "center", borderRadius: "50%", position: "absolute", left: "3%", top: "40%" }}><img src={`SliderLeft.svg`} alt="" style={{position: "absolute", top:"30%",right:"40%",left:"30%",bottom:"30%",transform:"scaleX(-1)"}} /></button>
                     </div>
-                    <div></div>
+                    <div className="d-flex flex-row" style={{justifyContent:"space-between",marginTop:"23px"}}>
+                        <div style={{width:"26.5%"}}>
+                            <img src={arr[1]} className="img-fluid" style={{height:"113px",width:"100%"}} alt="" />
+                        </div>
+                        <div style={{width:"26.5%"}}>
+                             <img src={arr[2]} className="img-fluid" style={{height:"113px",width:"100%"}} alt="" />
+                        </div>
+                        <div style={{ width: "26.5%",position: "relative"}}>
+                            <div style={{position:"absolute", padding:"14% 39%",background:"rgb(0,0,0,0.48)" ,color:"white"}}> <h1> +{arr.length-1-3}</h1></div>
+                            <img src={arr[3]} className="img-fluid" style={{height:"113px",width:"100%"}} alt="" />
+                        </div>
+                    </div>
                 </div>
 
                 <div className="d-flex flex-column" style={{width:"29.65%",marginRight:"1.67%",background: "#FFFFFF",boxShadow: "0px 4px 20px rgba(0, 47, 52, 0.12)"}}>
@@ -47,10 +86,10 @@ export const ViewYourAdd = () => {
                             <p style={{ margin:"24px 32px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"500",fontSize:"18px",color:"#002F34" }}>Physical Condition</p>
                         </div>
                         <div className="col-6" style={{textAlign: 'start'}}>
-                             <p style={{ margin:"24px 32px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"normal",fontSize:"18px",color:"#002F34" }}>Brand Name</p>
+                            <p style={{ margin:"24px 32px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"normal",fontSize:"18px",color:"#002F34" }}>Brand Name</p>
                             <p style={{ margin:"24px 32px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"normal",fontSize:"18px",color:"#002F34" }}>Year of purchase</p>
                             <p style={{ margin:"24px 32px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"normal",fontSize:"18px",color:"#002F34" }}>Model</p>
-                            <p style={{ margin:"24px 32px",marginBottom:"149px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"normal",fontSize:"18px",color:"#002F34" }}>Physical Condition</p>
+                            <p style={{ margin:"24px 32px",fontFamily: "Graphik",fontStyle: "normal",fontWeight:"normal",fontSize:"18px",color:"#002F34" }}>Physical Condition</p>
                         </div>
                     </div>
 

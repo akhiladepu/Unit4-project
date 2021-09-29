80421bb3d48cce6d9f04e70ed8bd4999ccbd994c
Akhil Kumar Adepu <akhil9394021176@gmail.com>
2021-09-29 21:28:44 +0530

    clearing the staging area

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { CarouselComponent } from './Components/HomePage/CarouselComponent/CarouselComponent.jsx';
 import { HomePage } from './Components/HomePage/HomePage.jsx';
 
 function App() {
   return (
     <div className="App">
         OLX Home Page
-        <HomePage/>
+      <HomePage />
+      {/* <CarouselComponent/> */}
     </div>
   );
 }

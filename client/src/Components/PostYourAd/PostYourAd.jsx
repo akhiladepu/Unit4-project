04454042e7c24152bbf7fae87db5b69f9e0b46c0
cybervirus997 <soumyaranjanpurohit46@gmail.com>
2021-09-30 21:54:08 +0530

    done with basic and implemented functionality part

--- client/src/Components/PostYourAd/PostYourAd.jsx
+++ client/src/Components/PostYourAd/PostYourAd.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
-import { Headd,InpHead,InpBar,Downarrow,Footer } from './PostYourAddbootStrap';
+import React, {useState} from 'react';
+import "./PostYourAd.css"
+import { Headd, InpHead, InpBar, Downarrow, Footer, ImgTaker, FootButton, SelectBar, OptionBar } from './PostYourAddbootStrap';
+import ImageUploading from "react-images-uploading";
+
+
+const initState = {
+    "brandName": "",
+    "yearOfPurchase": "",
+    "model": "",
+    "physicalCondition": "",
+    "adTitle": "",
+    "descrption": "",
+    "price": "",
+    "pincode":""
+}
 
 export const PostYourAd = () => {
+
+    const [text, setText] = useState(initState);
+    const [totalData, setTotalData] = useState([]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setText({...text,[name]: value});
+    }
+
+    const handleInput = () => {
+        setTotalData([...totalData,text])
+    }
+
+
+    const [images, setImages] = React.useState([]);
+    const maxNumber = 69;
+    const onChange = (imageList, addUpdateIndex) => {
+        // data for submit
+        console.log(imageList, addUpdateIndex);
+        setImages(imageList);
+    };
+
     return (
         <div className="col-12">
             <Headd className="col-12 d-flex flex-row">
@@ -19,34 +55,168 @@ export const PostYourAd = () => {
                 <img src={`/POSTYOURAD/UploadProductImages.svg`} alt="" style={{marginBottom:"8px",marginLeft:'355px',marginTop:'40px',Width:"417px",Height:"64px"}} />
             </div>
 
-            <div className="d-flex flex-column">
-                <div>
-                    <InpHead>Brand name *</InpHead>
-                    <Downarrow> <img src={`/POSTYOURAD/sliderLeft.svg`} style={{transform: 'rotate(90deg)'}} alt="" /> </Downarrow>
-                    <InpBar type="text" name="" id="">
-                    </InpBar>
+            <div className="d-flex flex-row">
+
+                <div className="d-flex flex-column">
+                    <div>
+                        <InpHead>Brand name *</InpHead>
+                        
+                        <SelectBar className="form-select" name="brandName" onChange={handleChange}>
+                            <OptionBar value=""></OptionBar>
+                            <OptionBar value="Canon">Canon</OptionBar>
+                            <OptionBar value="Sony">Sony</OptionBar>
+                            <OptionBar value="Nikon">Nikon</OptionBar>
+                            <OptionBar value="Samsung">Samsung</OptionBar>
+                            <OptionBar value="Panasonic">Panasonic</OptionBar>
+                        </SelectBar>
+                    </div>
+                    <div>
+                        <InpHead>Year of Purchase *</InpHead>
+                        <SelectBar className="form-select" type="text" name="yearOfPurchase" onChange={handleChange}>
+                            <OptionBar value=""></OptionBar>
+                            <OptionBar value="2020">2020</OptionBar>
+                            <OptionBar value="2019">2019</OptionBar>
+                            <OptionBar value="2018">2018</OptionBar>
+                            <OptionBar value="2017">2017</OptionBar>
+                            <OptionBar value="2016">2016</OptionBar>
+                        </SelectBar>
+                    </div>
+                    <div>
+                        <InpHead>Model *</InpHead>
+                        <InpBar type="text" name="model" onChange={handleChange}>
+                        </InpBar>
+                    </div>
+                    <div>
+                        <InpHead>Physical Condition *</InpHead>
+                        <SelectBar className="form-select" type="text" name="physicalCondition" onChange={handleChange}>
+                            <OptionBar value=""></OptionBar>
+                            <OptionBar value="Almost Like new">Almost Like new</OptionBar>
+                            <OptionBar value="Minor Scratches">Minor Scratches</OptionBar>
+                            <OptionBar value="Critical but reparable">Critical but reparable</OptionBar>
+                            <OptionBar value="Broken">Broken</OptionBar>
+                            <OptionBar value="Internal Damages">Internal Damages</OptionBar>
+                        </SelectBar>
+                    </div>
+                    <div>
+                        <InpHead>Ad Title *</InpHead>
+                        <InpBar type="text" name="adTitle" onChange={handleChange}></InpBar>
+                    </div>
+                    <div>
+                        <InpHead>Description *</InpHead>
+                        <InpBar type="text" name="descrption" onChange={handleChange}></InpBar>
+                        
+                    </div>
                 </div>
-                <div>
-                    <InpHead>Year of Purchase *</InpHead>
-                    <Downarrow> <img src={`/POSTYOURAD/sliderLeft.svg`} style={{transform: 'rotate(90deg)'}} alt="" /> </Downarrow>
-                    <InpBar type="text" name="" id=""></InpBar>
+                
+                <div style={{ width: "652px",marginLeft:"113px" }}>
+                    <InpHead style={{ marginLeft: "0px" }}>Description *</InpHead>
+                    <div className="col-12 d-flex flex-row" style={{flexFlow:"row wrap",justifyContent:"space-between"}}>
+
+                        <ImgTaker>
+                            <div style={{ margin: "71px 80.5px", position: "relative" }}>
+                               
+                                {/* juggad */}
+                                <div>
+                                     <ImageUploading
+                                    multiple
+                                    value={images}
+                                    onChange={onChange}
+                                    maxNumber={maxNumber}
+                                    dataURLKey="data_url"
+                                >
+                                    {({
+                                    imageList,
+                                    onImageUpload,
+                                    onImageRemoveAll,
+                                    onImageUpdate,
+                                    onImageRemove,
+                                    isDragging,
+                                    dragProps
+                                    }) => (
+                                    // write your building UI
+                                    <div className="upload__image-wrapper">
+                                        <button
+                                        style={isDragging ? { color: "red" } : null}
+                                        onClick={onImageUpload}
+                                        {...dragProps}
+                                        >
+                                        Click or Drop here
+                                        </button>
+                                        &nbsp;
+                                        <button onClick={onImageRemoveAll}>Remove all images</button>
+                                        {imageList.map((image, index) => (
+                                        <div key={index} className="image-item">
+                                            <img src={image.data_url} alt="" width="100" />
+                                            <div className="image-item__btn-wrapper">
+                                            <button onClick={() => onImageUpdate(index)}>Update</button>
+                                            <button onClick={() => onImageRemove(index)}>Remove</button>
+                                            </div>
+                                        </div>
+                                        ))}
+                                    </div>
+                                    )}
+                                </ImageUploading>
+                                </div>
+                                {/* jugaad */}
+                                
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker>
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+                        <ImgTaker >
+                            <div style={{ margin: "71px 80.5px",position: "relative"}}>
+                                <div style={{ border: "2px solid black",width:"15px",height:"15px",borderRadius: "50%",position: "absolute",top:"10px",left:"13px"}}></div>
+                                <img src={`/POSTYOURAD/Cameravector.svg`} alt="" />
+                            </div>
+                        </ImgTaker>
+
+                    </div>
                 </div>
-                <div>
-                    <InpHead>Model *</InpHead>
-                    <InpBar type="text" name="" id=""></InpBar>
-                </div>
-                <div>
-                    <InpHead>Physical Condition *</InpHead>
-                    <InpBar type="text" name="" id=""></InpBar>
-                </div>
-                <div>
-                    <InpHead>Ad Title *</InpHead>
-                    <InpBar type="text" name="" id=""></InpBar>
-                </div>
-                <div>
-                    <InpHead>Description *</InpHead>
-                    <InpBar type="text" name="" id=""></InpBar>
-                </div>
+
+                
+
             </div>
 
             <div>
@@ -56,16 +226,25 @@ export const PostYourAd = () => {
 
             <div className="d-flex flex-row" style={{marginBottom:"104px"}}>
                 <div>
-                    <InpHead>Description *</InpHead>
-                    <InpBar type="text" name="" id=""></InpBar>
+                    <InpHead>Price *</InpHead>
+                    <InpBar type="text" name="price" onChange={handleChange}></InpBar>
                 </div>
                 <div style={{marginLeft:"60px"}}>
-                    <InpHead>Description *</InpHead>
-                    <InpBar type="text" name="" id=""></InpBar>
+                    <InpHead>Pincode *</InpHead>
+                    <InpBar type="text" name="pincode" onChange={handleChange}></InpBar>
                 </div>
             </div>
             
+
+            <FootButton onClick={handleInput}>
+                <img src={`/POSTYOURAD/PostAd.svg`} alt="" style={{margin:"23px 110px"}} />
+            </FootButton>
+
             <Footer>
+                
+                <img src={`/POSTYOURAD/Sitemap.svg`} alt="" style={{marginLeft:"56px",marginTop:"21px",marginBottom:"23px",marginRight:"912px",width:"64px",height:"18px"}}  />
+                <img src={`/POSTYOURAD/FreeClassifiedsinIndia.svg`} alt="" style={{marginRight:"54px",marginTop:"21px",marginBottom:"23px"}}/>
+                <img src={ `/POSTYOURAD/20062021OLX.svg`} alt="" style={{marginRight:"56px",marginTop:"21px",marginBottom:"23px"}}/>
 
             </Footer>
 
--- client/src/Components/PostYourAd/PostYourAddbootStrap.jsx
+++ client/src/Components/PostYourAd/PostYourAddbootStrap.jsx
@@ -29,10 +29,54 @@ position:absolute;
 left:578px;
 top:233px;
 `
-const Footer = styled.div`
-width:100%;
-height:62px;
-background-color:rgba(0, 47, 52, 0.8);
+const SelectBar = styled.select`
+margin-left:56px;
+width:563px;
+height : 56px;
+border-radius :4px;
+border: 1px solid #002F34;
+&:hover {
+    border: 1px solid #002F34;
+  }
+  &:focus {
+      box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;;
+  }
+`
+const OptionBar = styled.option`
+margin-left:56px;
+width:563px;
+height : 56px;
+border-radius :4px;
 `
 
-export {Headd,InpHead,InpBar,Downarrow,Footer}
+const Footer = styled.div`
+width:100vw;
+
+background-color:rgba(0, 47, 52, 0.8);
+margin-bottom:0px;
+`
+
+const ImgTaker = styled.div`
+width:201px;
+height:173px;
+border:1px solid rgba(0, 47, 52, 0.7);
+margin-bottom:24px;
+background: #FFFFFF;
+border: 1px solid #002F34;
+box-sizing: border-box;
+border-radius: 4px;
+`
+
+const FootButton = styled.div`
+Width :312px;
+Height :72px;
+background: rgba(0, 127, 124, 0.1);
+border: 1px solid rgba(0, 127, 124, 0.6);
+box-sizing: border-box;
+border-radius: 4px;
+margin-top:104px;
+margin-left:564px;
+margin-bottom:96px;
+`
+
+export {Headd,InpHead,InpBar,SelectBar,Downarrow,Footer,ImgTaker,FootButton,OptionBar}

667a1f7af86b90804b295e11f378db5d7ed29d70
nikhil <ng3152340@gmail.com>
2021-10-01 09:59:01 +0530

    Completing the payment pages and ad not posted pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
-
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/Footer";     
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { HomePage } from './Components/HomePage/HomePage.jsx';
+import Footer from "./Components/Footer";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { HomePage } from "./Components/HomePage/HomePage.jsx";
+import AdNotPosted from "./Components/PostAnotherAd/AdNotPosted";
+import PaymentMethod from "./Components/PostAnotherAd/PaymentMethod";
+import CardMethod from "./Components/PostAnotherAd/CardMethod";
+import PaymentSuccessful from "./Components/PaymentSuccessful/PaymentSuccessful";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar />
       <HomePage />
-      <Footer />
+      <Footer /> */}
+      {/* <AdNotPosted /> */}
+      {/* <PaymentMethod /> */}
+      {/* <CardMethod /> */}
+      <PaymentMethod />
     </div>
   );
 }
